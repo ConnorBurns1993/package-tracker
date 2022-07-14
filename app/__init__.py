91eab32542869e8a3a4565e00663ae415f151799
Connor Burns <cburns1993@gmail.com>
2022-07-14 13:39:42 -0700

    finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from .config import Config
 from .shipping_form import ShippingForm
 from flask_migrate import Migrate
+from .models import db, Package
 import psycopg2
-from .models import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,28 +12,22 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    return "<h1>Package Tracker</h1>"
+    packages = Package.query.all()
+    return render_template('package_status.html', packages=packages)
 
-@app.route('/new-package', methods = ['GET, POST'])
+@app.route('/new-package', methods = ['GET', 'POST'])
 def new_package():
     form = ShippingForm()
-
     if form.validate_on_submit():
-        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
-            with conn.cursor() as curs:
-                curs.execute(
-                    '''
-                    INSERT INTO packages(name, recipient, origin, destination, express_shipping)
-                    VALUES (%(name)s, %(recipient)s, %(origin)s, %(destination)s, %(express_shipping)s)
-                    ''',
-                    {
-                    'name': form.name.data,
-                    'recipient': form.recipient.data,
-                    'origin': form.origin.data,
-                    'destination': form.destination.data,
-                    'express_shipping': form.express_shipping.data
-                    }
-                )
-                return redirect('/')
+        data = form.data
+        new_package = Package(sender=data["name"],
+                              recipient=data["recipient"],
+                              origin=data["origin"],
+                              destination=data["destination"],
+                              location=data["origin"])
+        db.session.add(new_package)
+        db.session.commit()
+        Package.advance_all_locations()
+        return redirect('/')
 
-    return render_template('shipping_request.html')
+    return render_template('shipping_request.html', form=form)
--- app/shipping_form.py
+++ app/shipping_form.py
@@ -7,7 +7,7 @@ class ShippingForm(FlaskForm):
     name = StringField("From", validators=[DataRequired()])
     recipient = StringField("To", validators=[DataRequired()])
     origin = SelectField("Origin", validators=[DataRequired()], choices = map.items())
-    destination = SelectField("Destination", validators=[DataRequired()])
+    destination = SelectField("Destination", validators=[DataRequired()], choices = map.items())
     express_shipping = BooleanField("Express Shipping")
     submit = SubmitField("Submit")
     cancel = SubmitField("Cancel")
